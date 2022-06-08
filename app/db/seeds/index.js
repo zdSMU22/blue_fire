d9ef85d8a17d254098279ac581e50b9552246f94
Dixon <zdixon91.zd@gmail.com>
2022-06-08 08:17:19 -0500

    changing folder structure

--- app/db/seeds/index.js
+++
@@ -1,7 +0,0 @@
-const seedUsers = require('./user-seeds');
-
-const seedAll = () => Promise.all([
-                                     seedUsers()
-                                  ]);
-
-module.exports = seedAll;                             
