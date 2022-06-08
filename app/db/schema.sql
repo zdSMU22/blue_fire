7a50237f092ab62c292a583cea433b01bd80bd1e
Dixon <zdixon91.zd@gmail.com>
2022-06-07 19:15:09 -0500

    added table

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -1,5 +1,14 @@
--- DROP DATABASE
-DROP DATABASE IF EXISTS blue_fire_db;
+DROP DATABASE IF EXISTS bleufire_db;
+CREATE DATABASE bleufire_db;
 
--- CREATE DATABASE
-CREATE DATABASE blue_fire_db;
+USE bleufire_db;
+
+CREATE TABLE userinfo (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  FirstName VARCHAR(30) NOT NULL,
+  LastName VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  passwd VARCHAR(30) NOT NULL,
+);
+
+-- Should I create table categories? Is that needed? Do I set seeds seing that we will be getting that information from user input?
