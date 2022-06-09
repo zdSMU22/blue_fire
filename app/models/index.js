cb1e1d15e0868fb652d9f398ab312bdacd53b624
zdSMU22 <94766160+zdSMU22@users.noreply.github.com>
2022-06-09 17:08:53 -0500

    Update index.js
    removed old code

--- app/models/index.js
+++ app/models/index.js
@@ -1,10 +1,10 @@
 // import models
 const User = require('./User');
 const WorkoutJournal = require('./workoutJournal');
-const Painting = require('./Painting');
+
 
 
 
 module.exports = {
-    User, WorkoutJournal, Painting
+    User, WorkoutJournal
 };
