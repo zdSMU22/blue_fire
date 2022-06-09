e339a6c3e2b093081810f6b7d99d800fea7a6cb2
Dixon <zdixon91.zd@gmail.com>
2022-06-08 22:42:52 -0500

    merging code

--- app/db/seeds/index.js
+++ app/db/seeds/index.js
@@ -1,9 +1,11 @@
 const seedUsers = require('./user-seeds');
 const seedFitnessPlans = require('./fitnessplan-seeds');
+const seedworkoutJournal = require('./workoutJournalData');
 
 const seedAll = () => Promise.all([
                                      seedUsers(),
-                                     seedFitnessPlans()
+                                     seedFitnessPlans(),
+                                     seedworkoutJournal()
                                   ]);
 
 module.exports = seedAll;                             
