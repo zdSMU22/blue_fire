ace1d7e58d4dfa909abde6cc8c40145cb7c105ab
Dixon <zdixon91.zd@gmail.com>
2022-06-08 22:50:38 -0500

    changes

---
+++ app/models/workoutJournal.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class workoutJournal extends Model {}
+
+workoutJournal.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    exercise_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    exercise_minutes: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    exercise_calories: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    exercise_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    total_calories: {
+      type: DataTypes.VIRTUAL,
+      get(){
+        return this.exercise_minutes * this.exercise_calories;
+      }
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'workoutJournal',
+  }
+);
+
+WorkoutJournal
+
+module.exports = WorkoutJournal;
